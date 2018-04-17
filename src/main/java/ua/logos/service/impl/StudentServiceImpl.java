fec1e56cffb4362b26bffae5a61c034c331b18f4
ащч <@SAMSUNG350U2B>
2018-04-17 17:29:36 +0300

    fixed

--- src/main/java/ua/logos/service/impl/StudentServiceImpl.java
+++ src/main/java/ua/logos/service/impl/StudentServiceImpl.java
@@ -3,11 +3,13 @@ package ua.logos.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import ua.logos.entity.Student;
 import ua.logos.repository.StudentRepository;
 import ua.logos.service.StudentService;
 
+@Service
 public class StudentServiceImpl implements StudentService {
 
 	@Autowired
