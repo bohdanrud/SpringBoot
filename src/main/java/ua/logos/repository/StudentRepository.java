a3cb3c5a0cfd1ceb1b52fe017fbf66208adc524b
ащч <@SAMSUNG350U2B>
2018-04-19 16:12:39 +0300

    JpaSpecificationExecutor

--- src/main/java/ua/logos/repository/StudentRepository.java
+++ src/main/java/ua/logos/repository/StudentRepository.java
@@ -1,11 +1,14 @@
 package ua.logos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import ua.logos.entity.Student;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Integer>{
+public interface StudentRepository extends JpaRepository<Student, Integer>, JpaSpecificationExecutor<Student> {
 
+	
+	
 }
